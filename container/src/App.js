d3877b119825c0acc8c59960c3aef38a39f74bae
Vijay Sharma <Vijay.Sharma@causeway.com>
2024-11-27 18:59:04 +0530

    trigger build

--- container/src/App.js
+++ container/src/App.js
@@ -2,5 +2,10 @@ import React from "react";
 import MarketingApp from "./component/MarketingApp";
 
 export default () => {
-  return <MarketingApp />;
+  return (
+    <>
+      <h2>Hello there!</h2>
+      <MarketingApp />
+    </>
+  );
 };
