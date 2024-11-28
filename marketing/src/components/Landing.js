41a212cdddeb533f647bfccc7b0757b9a8483495
Vijay Sharma <Vijay.Sharma@causeway.com>
2024-11-28 20:48:43 +0530

    trigger build

--- marketing/src/components/Landing.js
+++ marketing/src/components/Landing.js
@@ -79,7 +79,7 @@ export default function Album() {
               color="textPrimary"
               gutterBottom
             >
-              Marketing Home Page
+              Home Page
             </Typography>
             <Typography
               variant="h5"
